9f4b59ce73c3f61f88b9259ca215f26461616363
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-10-27 12:54:11 +0100

    completed

--- src/7kyu/youre-a-square.ts
+++ src/7kyu/youre-a-square.ts
@@ -1,16 +1,16 @@
 // https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
 
 function square(number: number): boolean {
-    let list = Array.from({length: 10000}, (item, index) => index)
-    let squareNumbers = []
-    for (let i of list){
-        squareNumbers.push(i*i)
-    }
-    if (squareNumbers.includes(number)){
-        return true
-    } else {
-        return false
-    }
+  const list = Array.from({ length: 10000 }, (item, index) => index);
+  const squareNumbers = [];
+  for (const i of list) {
+    squareNumbers.push(i * i);
+  }
+  if (squareNumbers.includes(number)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(square(4), "expected = true")
+console.log(square(4), "expected = true");
