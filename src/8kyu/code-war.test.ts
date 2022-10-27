1a7678ed38dd1ee6ee9ef0d96e8efad29e966ad4
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-10-27 13:15:54 +0100

    chnages

--- src/8kyu/code-war.test.ts
+++ src/8kyu/code-war.test.ts
@@ -1,4 +1,4 @@
-import containsValue from "../code-war";
+//import containsValue from "../code-war";
 
 test("find if an array contains a certain value", () => {
   expect(containsValue([1, 2, 3, 4], 4)).toBe(true);
