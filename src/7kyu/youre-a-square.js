410a9d38628d5205e898c33baafdf862d11b75f6
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-10-27 12:58:48 +0100

    fixed checks

--- src/7kyu/youre-a-square.js
+++ src/7kyu/youre-a-square.js
@@ -1,16 +1,15 @@
 "use strict";
 // https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
 function square(number) {
-    const list = Array.from({ length: 10000000000 }, (item, index) => index);
-    const squareNumbers = [];
-    for (const i of list) {
-        squareNumbers.push(i * i);
-    }
-    if (squareNumbers.includes(number)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+  const list = Array.from({ length: 10000000000 }, (item, index) => index);
+  const squareNumbers = [];
+  for (const i of list) {
+    squareNumbers.push(i * i);
+  }
+  if (squareNumbers.includes(number)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 console.log(square(4), "expected = true");
